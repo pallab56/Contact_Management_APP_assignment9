46126fc4976c61f07a061a1307eca8da73cee174
Pallab Chandra Das <pallab15-5698@diu.edu.bd>
2026-08-18 19:37:09 +0600

    database created

--- lib/db/db_handler.dart
+++ lib/db/db_handler.dart
@@ -1,3 +1,51 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
 class DbHandler {
+  static final DbHandler instance = DbHandler._constructor();
+  final String contactTableName = 'contact';
+
+  //column name
+  final String contactIdColumnName = 'id';
+
+  final String contactNameColumnName = 'name';
+  final String contactNumberColumnName = 'number';
+  final String contactEmailColumnName = 'email';
+  final String contactAddressColumnName = 'address';
+  final String contactFavoriteColumnName = 'isFavorite';
+  DbHandler._constructor();
   
+  Database? db;
+  Future<Database> get database async {
+    if (db != null) {
+      return db!;
+    }
+    db = await getDatabase();
+    return db!;
+  }
+
+  Future<Database> getDatabase() async {
+    final databaseDirPath = await getDatabasesPath();
+
+    final databasePath = join(databaseDirPath, 'contact_mager_db.db');
+
+    final database = await openDatabase(
+      databasePath,
+      version: 1,
+      onCreate: (db, version) {
+        db.execute('''
+      CREATE TABLE $contactTableName(
+      
+      $contactIdColumnName INTEGER PRIMARY KEY AUTOINCREMENT,
+      $contactNameColumnName TEXT NOT NULL,
+      $contactNumberColumnName TEXT NOT NULL,
+      $contactEmailColumnName TEXT NOT NULL,
+      $contactAddressColumnName TEXT NOT NULL,
+      $contactFavoriteColumnName INTEGER NOT NULL DEFAULT 0
+      )
+''');
+      },
+    );
+    return database;
+  }
 }
